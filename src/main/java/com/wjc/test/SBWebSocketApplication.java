4162847921acc943c9808028a56b21fa6bd7a6b5
wjc1243 <wjc1243@163.com>
2018-08-19 11:40:44 +0800

    websocket初始化

---
+++ src/main/java/com/wjc/test/SBWebSocketApplication.java
@@ -0,0 +1,11 @@
+package com.wjc.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SBWebSocketApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SBWebSocketApplication.class, args);
+    }
+}
